b1ba80c04ef1c87242aad808580ba47e051d7d4d
Hao-Wei Pang <hwpang@mit.edu>
2023-04-15 16:23:17 -0400

    adapt aspen condition extract script for oxygen case
    adapt
    
    adapt
    
    font
    
    remove folder
    
    adap
    
    oxygen extract
    
    adapt
    
    fix extract

--- debutanizer_models/basecase/aspen_simulation/extract_aspen_conditions.jl
+++ debutanizer_models/basecase/aspen_simulation/extract_aspen_conditions.jl
@@ -12,19 +12,21 @@ merge!(rcParams, font0)
 # %%
 ENV["COLUMNS"] = 500
 ENV["ROWS"] = 500
-filename = "Distillation_FEDP_XN_V2_060720_v2.xlsx"
+
+aspen_results_path = ARGS[1]
+model_name = ARGS[2]
 
 # %%
-TPFQ = DataFrame(XLSX.readtable(filename,"TPFQ"))
+TPFQ = DataFrame(XLSX.readtable(aspen_results_path,"TPFQ"))
 
 # %%
-liquid_molfraction = DataFrame(XLSX.readtable(filename,"LiquidCompositionMoleBasis"))
+liquid_molfraction = DataFrame(XLSX.readtable(aspen_results_path,"LiquidCompositionMoleBasis"))
 
 # %%
-vapor_molfraction = DataFrame(XLSX.readtable(filename,"VaporCompositionMoleBasis"))
+vapor_molfraction = DataFrame(XLSX.readtable(aspen_results_path,"VaporCompositionMoleBasis"))
 
 # %%
-stream = DataFrame(XLSX.readtable(filename,"StreamResults"))
+stream = DataFrame(XLSX.readtable(aspen_results_path,"StreamResults"))
 
 # %%
 spcnamedict = Dict(
@@ -46,6 +48,11 @@ spcnames = [
     "TOLUENE",
     "STYRENE"
 ]
+
+if model_name == "trace_oxygen_perturbed_debutanizer_model"
+    spcnamedict["OXYGEN"] = "OXYGEN"
+    push!(spcnames,"OXYGEN")
+end
 
 # %%
 vapor_molfraction
@@ -73,11 +80,20 @@ initial_conditions["P"] = P
 initial_conditions["liquid_concentration"] = Dict()
 initial_conditions["vapor_concentration"] = Dict()
 for (aspenname, spcname) in spcnamedict
-    initial_conditions["liquid_concentration"][spcname] = liquid_concentration[:,aspenname]
-    initial_conditions["vapor_concentration"][spcname] = vapor_concentration[:,aspenname]
+    conc = liquid_concentration[:,aspenname]
+    conc[conc .< 1e-18] .= 0.0
+    initial_conditions["liquid_concentration"][spcname] = conc
+    conc = vapor_concentration[:,aspenname]
+    conc[conc .< 1e-18] .= 0.0
+    initial_conditions["vapor_concentration"][spcname] = conc
 end
 
-YAML.write_file("aspen_conditions.yml",initial_conditions)
+if model_name == "basecase_debutanizer_model"
+    save_name = "aspen_conditions"
+elseif model_name == "trace_oxygen_perturbed_debutanizer_model"
+    save_name = "aspen_conditions_oxygen"
+end
+YAML.write_file("$save_name.yml", initial_conditions)
 
 # %%
 d = 2.5
@@ -89,7 +105,12 @@ Vvap = A*(spacing-h)
 
 lines = ["-v", "-^", "-<", "->", "-1", "-2", "-3", "-4", "-8", "-s", "-p", "-P", "-*", "-h", "-H", "-+", "-x", "-X", "-D", "-d",]
 
-fig,axs = subplots(nrows=4,ncols=2,figsize=(10,14))
+if model_name == "basecase_debutanizer_model"
+    fig,axs = subplots(nrows=4,ncols=2,figsize=(10,14))
+elseif model_name == "trace_oxygen_perturbed_debutanizer_model"
+    fig,axs = subplots(nrows=5,ncols=2,figsize=(10,17))
+end
+
 fig.subplots_adjust(wspace=0, hspace=0)
 
 axs[1,1].plot(1:40,initial_conditions["T"],color="k","-o")
@@ -98,7 +119,8 @@ axs[1,1].set_title("(a)",loc="left",size=20)
 axs[1,1].set_xticklabels([])
 
 for (i,spc) in enumerate(spcnames)
-    axs[2,1].plot(1:40,initial_conditions["liquid_concentration"][spc],lines[i],label=spc)
+    label = spc
+    axs[2,1].plot(1:40,initial_conditions["liquid_concentration"][spc],lines[i],label=label)
 end
 
 axs[2,1].set_ylabel("Conc. (mol/m^3)",size=20)
@@ -110,7 +132,8 @@ axs[2,1].plot([41,41],[-400,800],"k--")
 axs[2,1].plot([0,41],[800,800],"k--")
 
 for (i,spc) in enumerate(spcnames)
-    axs[3,1].plot(1:40,initial_conditions["liquid_concentration"][spc],lines[i],label=spc)
+    label = spc
+    axs[3,1].plot(1:40,initial_conditions["liquid_concentration"][spc],lines[i],label=label)
 end
 axs[3,1].set_ylabel("Conc. (mol/m^3)",size=20)
 axs[3,1].set_ylim([-20,600])
@@ -119,7 +142,8 @@ axs[3,1].set_xticklabels([])
 
 
 for (i,spc) in enumerate(spcnames)
-    axs[2,2].plot(1:40,initial_conditions["vapor_concentration"][spc],lines[i],label=spc)
+    label = spc
+    axs[2,2].plot(1:40,initial_conditions["vapor_concentration"][spc],lines[i],label=label)
 end
 axs[2,2].set_title("(c) Vapor phase",loc="left",size=20)
 axs[2,2].set_xticklabels([])
@@ -129,11 +153,12 @@ axs[2,2].plot([41,41],[-10,20],"k--")
 axs[2,2].plot([0,41],[20,20],"k--")
 handles, labels = axs[2,2].get_legend_handles_labels()
 
-axs[1,2].legend(handles=handles, loc="center",fontsize=18)
+axs[1,2].legend(handles=handles, loc="center",fontsize=14)
 axs[1,2].axis("off")
 
 for (i,spc) in enumerate(spcnames)
-    axs[3,2].plot(1:40,initial_conditions["vapor_concentration"][spc],lines[i],label=spc)
+    label = spc
+    axs[3,2].plot(1:40,initial_conditions["vapor_concentration"][spc],lines[i],label=label)
 end
 axs[3,2].set_ylim([-1,20])
 axs[3,2].set_title("Zoomed in on (c)",loc="left",size=20)
@@ -141,26 +166,52 @@ axs[3,2].set_xticklabels([])
 
 flowrate = initial_conditions["liquid_outlet_volumetric_flowrate"][:]
 flowrate[end] = flowrate[end-1]
-axs[4,1].plot(1:40,Vliq ./ flowrate,"-o")
+axs[4,1].plot(1:40,Vliq ./ flowrate,color="k","-o")
 axs[4,1].set_ylabel("Residence time (s)",size=20)
 axs[4,1].set_title("(d) Liquid phase",loc="left",size=20)
-axs[4,1].set_xlabel("Tray",size=20)
+if model_name == "basecase_debutanizer_model"
+    axs[4,1].set_xlabel("Tray",size=20)
+elseif model_name == "trace_oxygen_perturbed_debutanizer_model"
+    axs[4,1].set_xticklabels([])
+end
 
 flowrate = initial_conditions["vapor_outlet_volumetric_flowrate"][:]
 flowrate[1] = flowrate[2]
-axs[4,2].plot(1:40,Vvap ./ flowrate,"-o")
+axs[4,2].plot(1:40,Vvap ./ flowrate,color="k","-o")
 axs[4,2].set_title("(e) Vapor phase",loc="left",size=20)
-axs[4,2].set_xlabel("Tray",size=20)
+if model_name == "basecase_debutanizer_model"
+    axs[4,2].set_xlabel("Tray",size=20)
+elseif model_name == "trace_oxygen_perturbed_debutanizer_model"
+    axs[4,2].set_xticklabels([])
+end
+
+if model_name == "trace_oxygen_perturbed_debutanizer_model"
+    spc = "OXYGEN"
+    i = findfirst(x->x==spc,spcnames)
+    label = spc
+    axs[5,1].plot(1:40,initial_conditions["liquid_concentration"][spc],lines[i],label=spc,color="C8")
+    axs[5,1].set_ylabel("Conc. (mol/m^3)",size=20)
+    axs[5,1].set_title("(f) Liquid phase",loc="left",size=20)
+    axs[5,1].set_xlabel("Tray",size=20)
+    axs[5,1].set_yscale("log")
+    axs[5,1].set_ylim([1e-18, 1e0])
+
+    axs[5,2].plot(1:40,initial_conditions["vapor_concentration"][spc],lines[i],label=spc,color="C8")
+    axs[5,2].set_title("(g) Vapor phase",loc="left",size=20)
+    axs[5,2].set_xlabel("Tray",size=20)
+    axs[5,2].set_yscale("log")
+    axs[5,1].set_ylim([1e-18, 1e0])
+end
 
 fig.tight_layout()
 
-figure_folder = "Figures"
-if !isdir(figure_folder)
-    mkdir(figure_folder)
-end
-
 plt.tight_layout()
-savefig("./Figures/monomer_conc_trays.pdf",bbox_inches="tight")
+if model_name == "basecase_debutanizer_model"
+    save_name = "monomer_conc_trays"
+elseif model_name == "trace_oxygen_perturbed_debutanizer_model"
+    save_name = "monomer_conc_trays_oxygen"
+end 
+savefig("$save_name.pdf",bbox_inches="tight")
 
 # %%
 
