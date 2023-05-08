f4ce23042692bb3ae2aa8c07a231b5c850bf3263
Hao-Wei Pang <hwpang@mit.edu>
2023-05-08 18:02:53 -0400

    update

--- debutanizer_models/trace_oxygen_perturbed/analysis/submit_analyze_debutanizer.sh
+++ debutanizer_models/trace_oxygen_perturbed/analysis/submit_analyze_debutanizer.sh
@@ -7,10 +7,8 @@ conda activate rmg_py3_20230404
 which julia
 
 PFM_PATH=/home/gridsan/hwpang/Software/PolymerFoulingModeling
-rms_path=$PFM_PATH/debutanizer_models/trace_oxygen_perturbed/liquid_mechanism/chem.rms
+alpha_rates_path="../simulation_results/OXYGEN_1e0_3600.0_32.0/alpha_rates.yml"
 model_name=trace_oxygen_perturbed_debutanizer_model
-time_step=32.0
-julia $PFM_PATH/debutanizer_models/basecase/analysis/analyze_debutanizer_model.jl \
-        $rms_path \
-        $model_name \
-        $time_step
+julia $PFM_PATH/analyze_debutanizer_model.py \
+        --alpha_rates_path $alpha_rates_path \
+        --model_name $model_name
