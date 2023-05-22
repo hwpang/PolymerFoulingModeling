62c1f93e8dab154fc4fb247ca3c4d50f2e3f1aa1
Hao-Wei Pang <hwpang@mit.edu>
2023-05-22 14:52:31 -0400

    plot RC., ROO., R.

--- debutanizer_models/trace_oxygen_perturbed/analysis/submit_analyze_debutanizer.sh
+++ debutanizer_models/trace_oxygen_perturbed/analysis/submit_analyze_debutanizer.sh
@@ -10,10 +10,10 @@ PFM_PATH=/home/gridsan/hwpang/Software/PolymerFoulingModeling
 alpha_rates_path="../simulation_results/OXYGEN_1e0_3600.0_32.0/alpha_rates.yml"
 model_name=trace_oxygen_perturbed_debutanizer_model
 
-start=`date +%s`
-python $PFM_PATH/analyze_debutanizer_model.py \
+start=$(date +%s)
+python $PFM_PATH/debutanizer_models/trace_oxygen_perturbed/analysis/analyze_debutanizer_model.py \
         --alpha_rates_path $alpha_rates_path \
         --model_name $model_name
-end=`date +%s`
-runtime=$((end-start))
+end=$(date +%s)
+runtime=$((end - start))
 echo "runtime: $runtime"
