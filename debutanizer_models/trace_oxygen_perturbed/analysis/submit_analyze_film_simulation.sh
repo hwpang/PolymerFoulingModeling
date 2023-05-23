6414d0d0c6ddf807b7092474e8e1a81e9f0a6fd6
Hao-Wei Pang <hwpang@mit.edu>
2023-05-23 14:25:22 -0400

    fix

--- debutanizer_models/trace_oxygen_perturbed/analysis/submit_analyze_film_simulation.sh
+++ debutanizer_models/trace_oxygen_perturbed/analysis/submit_analyze_film_simulation.sh
@@ -10,6 +10,9 @@ PFM_PATH=/home/gridsan/hwpang/Software/PolymerFoulingModeling
 model_name=trace_oxygen_perturbed_debutanizer_model
 rms_path=$PFM_PATH/debutanizer_models/trace_oxygen_perturbed/liquid_mechanism/chem.rms
 
+start=$(date +%s)
 julia $PFM_PATH/debutanizer_models/basecase/analysis/analyze_film_simulation.jl \
         $model_name \
         $rms_path
+runtime=$((end - start))
+echo "runtime: $runtime"
