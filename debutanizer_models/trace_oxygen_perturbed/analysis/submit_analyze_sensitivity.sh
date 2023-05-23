960279bad95d6f05ed456233d3509b0afaf70be7
Hao-Wei Pang <hwpang@mit.edu>
2023-05-23 17:34:19 -0400

    fix

---
+++ debutanizer_models/trace_oxygen_perturbed/analysis/submit_analyze_sensitivity.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -l
+#SBATCH -J analyze_film_simulation
+#SBATCH -o slurm-analyze_film_simulation.out
+
+conda activate rmg_py3_20230404
+
+which julia
+which python
+
+PFM_PATH=/home/gridsan/hwpang/Software/PolymerFoulingModeling
+model_name=trace_oxygen_perturbed_debutanizer_model
+all_simulation_directory="../simulation_results"
+
+start=$(date +%s)
+python $PFM_PATH/debutanizer_models/trace_oxygen_perturbed/analysis/analyze_film_simulation.py \
+        --model_name $model_name \
+        --all_simulation_directory $all_simulation_directory
+end=$(date +%s)
+runtime=$((end - start))
+echo "runtime: $runtime"
