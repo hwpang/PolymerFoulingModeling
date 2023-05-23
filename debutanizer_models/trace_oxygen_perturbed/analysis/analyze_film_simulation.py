8dea1d7a77203ad4f0fbfac2b07fbedeb5d40933
Hao-Wei Pang <hwpang@mit.edu>
2023-05-23 15:13:53 -0400

    fix

--- debutanizer_models/trace_oxygen_perturbed/analysis/analyze_film_simulation.py
+++ debutanizer_models/trace_oxygen_perturbed/analysis/analyze_film_simulation.py
@@ -78,16 +78,38 @@ for tray in trays:
 
 print("Plot rate of film growth")
 
+def get_rops(df, rop_name, loss_only=False, production_only=False, N=5):
+    name_inds = df["rop_spcname"] == rop_name
+    rop_rxncomments = df.loc[name_inds, "rop_rxncomment"]
+    # rop_rxncomments = [rxnstr.replace("\n", " ") for rxnstr in rop_rxncomments]
+    # rop_rxncomments = [rxnstr.replace(" H abstraction", "") for rxnstr in rop_rxncomments]
+    rop_rxnstrs = df.loc[name_inds, "rop_rxnstr"]
+    rops = df.loc[name_inds, "rop"]
+    
+    if loss_only:
+        loss_inds = rops < 0
+        rops = rops[loss_inds]
+        rop_rxncomments = rop_rxncomments[loss_inds]
+        rop_rxnstrs = rop_rxnstrs[loss_inds]
+    elif production_only:
+        prod_inds = rops > 0
+        rops = rops[prod_inds]
+        rop_rxncomments = rop_rxncomments[prod_inds]
+        rop_rxnstrs = rop_rxnstrs[prod_inds]
+    
+    sorted_inds = sorted(range(len(rops)), key=lambda i: abs(rops[i]), reverse=True)
+    if len(sorted_inds) > N:
+        sorted_inds = sorted_inds[:N]
+    
+    return rops[sorted_inds], rop_rxncomments[sorted_inds], rop_rxnstrs[sorted_inds]
+
 nrows = len(selected_trays)
 ncols = 1
 
 fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(9, 12), sharex=True)
 
-all_rop_rxncomments = []
-
 for ind, tray in enumerate(selected_trays):
-    rops, rop_rxncomments, rop_rxnstrs = get_rops(film_rops[perturb_species, perturb_factor][tray], "mass")
-    all_rop_rxncomments.extend(rop_rxnstrs)
+    rops, rop_rxncomments, rop_rxnstrs = get_rops(rate_of_productions[tray], "mass")
     xs = np.arange(len(rop_rxncomments))
     mass = film_simulations[perturb_species, perturb_factor][tray][-1, "mass"]
     axs[ind].barh(xs, rops / mass, align="center")
