dff34440456fdc17c98495188cea86a6159d8caf
Hao-Wei Pang <hwpang@mit.edu>
2023-05-26 13:49:54 -0400

    add oxygen studies

--- debutanizer_models/basecase/film_simulation/simulate_film_submodel.jl
+++ debutanizer_models/basecase/film_simulation/simulate_film_submodel.jl
@@ -376,8 +376,23 @@ else
 
     @time sol = solve(react.ode, react.recommendedsolver, abstol=abstol, reltol=reltol)
 
+    println("Saving film phase submodel results...")
+
+    df = DataFrame(sol)
+    rename!(df, names(df)[domainliq.indexes[1]+1:domainliq.indexes[2]+1] .=> liqspcnames)
+    rename!(df, names(df)[domainfilm.indexes[1]+1:domainfilm.indexes[2]+1] .=> filmspcnames)
+    rename!(df, names(df)[domainfilm.indexes[3]+1] .=> "mass")
+
+    CSV.write("$(save_directory)/simulation_film_$(tray).csv", df)
+    save_rop(sol)
+
     if model_name == "trace_oxygen_perturbed_debutanizer_model"
         # get oxygen flux in mol/(m^3*s) at each time point
+        domainfilm, y0film, pfilm = FragmentBasedConstantTrhoDomain(phase=film, initialconds=filminitialconds)
+        domainliq, y0liq, pliq = ConstantTVDomain(phase=liq, initialconds=liqinitialconds)
+        inter, pinter = FragmentBasedReactiveFilmGrowthInterfaceConstantT(domainfilm, domainliq, interfacerxns)
+        react, y0, p = Reactor((domainfilm, domainliq), (y0film, y0liq), (0.0, tf), (inter,), (pfilm, pliq, pinter))
+
         oxygen_fluxes = zeros(length(sol.t))
         oxygen_concs = zeros(length(sol.t))
         oxygen_diffs = zeros(length(sol.t))
@@ -388,7 +403,7 @@ else
         for i in 1:length(sol.t)
             t = sol.t[i]
             u = sol.u[i]
-            sol.prob.f(du, u, p, t)
+            react.ode.f(du, u, p, t)
 
             mass = u[end]
             Vsolidinfilm = mass / rho
@@ -404,16 +419,6 @@ else
         results = DataFrame(timestamp=sol.t, oxygen_flux=oxygen_fluxes, oxygen_conc=oxygen_concs, oxygen_diff=oxygen_diffs)
         CSV.write("$(save_directory)/simulation_film_$(tray)_oxygen.csv", results)
     end
-
-    println("Saving film phase submodel results...")
-
-    df = DataFrame(sol)
-    rename!(df, names(df)[domainliq.indexes[1]+1:domainliq.indexes[2]+1] .=> liqspcnames)
-    rename!(df, names(df)[domainfilm.indexes[1]+1:domainfilm.indexes[2]+1] .=> filmspcnames)
-    rename!(df, names(df)[domainfilm.indexes[3]+1] .=> "mass")
-
-    CSV.write("$(save_directory)/simulation_film_$(tray).csv", df)
-    save_rop(sol)
 end
 
 println("Done!")
