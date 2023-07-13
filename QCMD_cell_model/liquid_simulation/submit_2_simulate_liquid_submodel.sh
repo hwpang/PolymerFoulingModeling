b0aeba4b8760b3aac4b8a4aa57ee48d1054ffa30
hwpang <hwpang@mit.edu>
2023-07-13 14:42:49 -0400

    Add script to simulate vapor/liquid phase

--- QCMD_cell_model/liquid_simulation/submit_2_simulate_liquid_submodel.sh
+++ QCMD_cell_model/liquid_simulation/submit_2_simulate_liquid_submodel.sh
@@ -16,25 +16,23 @@ rms_path=$PFM_PATH/QCMD_cell_model/liquid_mechanism/chem.rms
 jobs=()
 perturb_factor_list=("0.0" "1e-3" "1e-2" "1e-1" "1e0")
 
-for perturb_factor in "${perturb_factor_list[@]}"
-do
+for perturb_factor in "${perturb_factor_list[@]}"; do
     jobs+=("$perturb_factor")
 done
 
-for jobind in `seq $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT ${#jobs[@]}`
-do
+for jobind in $(seq $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT ${#jobs[@]}); do
     echo "SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
     echo "SLURM_ARRAY_TASK_COUNT: $SLURM_ARRAY_TASK_COUNT"
     echo "jobind: $jobind"
     echo "${jobs[$jobind]}"
     job=(${jobs[$jobind]})
     perturb_factor=${job[0]}
-    start=`date +%s`
+    start=$(date +%s)
     julia --threads 8 \
-            $PFM_PATH/QCMD_cell_model/liquid_simulation/simulate_liquid_submodel.jl \
-            $rms_path \
-            $perturb_factor
-    end=`date +%s`
-    runtime=$((end-start))
+        $PFM_PATH/QCMD_cell_model/liquid_simulation/simulate_liquid_submodel.jl \
+        $rms_path \
+        $perturb_factor
+    end=$(date +%s)
+    runtime=$((end - start))
     echo "runtime: $runtime"
 done
--- debutanizer_models/basecase/vapor_liquid_simulation/submit_3_simulate_vapor_liquid_submodels.sh
+++ debutanizer_models/basecase/vapor_liquid_simulation/submit_3_simulate_vapor_liquid_submodels.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -l
-#SBATCH -J simulate_vapor_liquid
-#SBATCH -o slurm-simulate_vapor_liquid-%a.out
+#SBATCH -o slurm-%x-%a.out
 #SBATCH -n 8
+#SBATCH -N 1
 #SBATCH --array=0-17
+
+echo "============================================================"
+echo "Job ID : $SLURM_JOB_ID"
+echo "Job Name : $SLURM_JOB_NAME"
+echo "Starting on : $(date)"
+echo "Running on node : $SLURMD_NODENAME"
+echo "Current directory : $(pwd)"
+echo "============================================================"
 
 conda activate rmg_py3_20230404
 
@@ -17,16 +25,13 @@ jobs=()
 perturb_species_list=("1,3-BUTADIENE" "CYCLOPENTADIENE")
 perturb_factor_list=("0.5" "0.7" "0.9" "1.0" "1.1" "1.3" "1.5" "1.7" "1.9")
 
-for perturb_species in "${perturb_species_list[@]}"
-do
-    for perturb_factor in "${perturb_factor_list[@]}"
-    do
+for perturb_species in "${perturb_species_list[@]}"; do
+    for perturb_factor in "${perturb_factor_list[@]}"; do
         jobs+=("$perturb_species $perturb_factor")
     done
 done
 
-for jobind in `seq $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT ${#jobs[@]}`
-do
+for jobind in $(seq $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT ${#jobs[@]}); do
     echo "SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
     echo "SLURM_ARRAY_TASK_COUNT: $SLURM_ARRAY_TASK_COUNT"
     echo "jobind: $jobind"
@@ -35,16 +40,16 @@ do
     perturb_species=${job[0]}
     perturb_factor=${job[1]}
     tray=${job[2]}
-    start=`date +%s`
+    start=$(date +%s)
     julia --threads 8 \
-            $PFM_PATH/debutanizer_models/basecase/vapor_liquid_simulation/simulate_vapor_liquid_submodels.jl \
-            $aspen_condition_path \
-            $rms_path \
-            $perturb_species \
-            $perturb_factor \
-            $tray \
-            64.0
-    end=`date +%s`
-    runtime=$((end-start))
+        $PFM_PATH/debutanizer_models/basecase/vapor_liquid_simulation/simulate_vapor_liquid_submodels.jl \
+        $aspen_condition_path \
+        $rms_path \
+        $perturb_species \
+        $perturb_factor \
+        $tray \
+        64.0
+    end=$(date +%s)
+    runtime=$((end - start))
     echo "runtime: $runtime"
 done
